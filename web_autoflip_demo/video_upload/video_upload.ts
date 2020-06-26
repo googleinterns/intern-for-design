a08467ca88286a3ea0e46df1b424f33e17381eae
Justin Parra <jrparra@google.com>
2020-06-25 17:02:56 -0700

    Adding typescript linting to the autoflip project folder and correcting the git ignores

--- web_autoflip_demo/video_upload/video_upload.ts
+++ web_autoflip_demo/video_upload/video_upload.ts
@@ -10,15 +10,15 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License. 
+limitations under the License.
 */
 
 // this function is called when the input loads a video
 function showVideo(): void {
   const file = (<HTMLInputElement>event.target).files[0];
-  //grab the first video in the fileList
-  //Currently, only loading one file
-  console.log("video file has been chosen");
+  // grab the first video in the fileList
+  // Currently, only loading one file
+  console.log('video file has been chosen');
   console.log(file);
   renderVideo(file);
 }
@@ -27,15 +27,16 @@ function showVideo(): void {
 function renderVideo(file: File): void {
   const reader = new FileReader();
   reader.onload = function () {
-    let the_url = reader.result;
-    // Using a template library like handlebars.js may be a better solution than just inserting a string
-    document.getElementById("data-vid").innerHTML =
-      "<video width='400' controls><source id='vid-source' src='" +
-      the_url +
-      "'type='video/mp4'></video>";
-    document.getElementById("name-vid").innerHTML = file.name;
+    const urlResult = reader.result as string;
+    // Using a template library like handlebars.js may be a better solution than
+    // just inserting a string
+    document.getElementById(
+      'data-vid',
+    ).innerHTML = `<video width='400' controls><source id='vid-source' src='${urlResult}'type='video/mp4'></video>`;
+
+    document.getElementById('name-vid').innerHTML = file.name;
   };
 
-  //when the file is read it triggers the onload event above.
+  // when the file is read it triggers the onload event above.
   reader.readAsDataURL(file);
 }
