2fcd851e7adc5d851986644a28ad330ea18e946e
Zhen Chen <zzhencchen@google.com>
2020-07-21 19:56:42 +0000

    Add lip track calculator, proto and unit test.

---
+++ autoflip_calculators/active_speaker_detection/lip_track_calculator.cc
@@ -0,0 +1,249 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <algorithm>
+#include <memory>
+#include <cmath>
+
+#include "mediapipe/examples/desktop/autoflip/autoflip_messages.pb.h"
+#include "mediapipe/examples/desktop/autoflip/calculators/lip_track_calculator.pb.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/formats/detection.pb.h"
+#include "mediapipe/framework/formats/rect.pb.h"
+#include "mediapipe/framework/formats/landmark.pb.h"
+#include "mediapipe/framework/port/ret_check.h"
+#include "mediapipe/framework/port/status.h"
+#include "mediapipe/framework/port/status_builder.h"
+#include "mediapipe/framework/port/opencv_core_inc.h"
+#include "mediapipe/framework/port/opencv_imgproc_inc.h"
+
+
+namespace mediapipe {
+namespace autoflip {
+
+constexpr char kInputLandmark[] = "LANDMARKS";
+constexpr char kInputROI[] = "ROIS_FROM_LANDMARKS";
+constexpr char kOutputROI[] = "ROIS";
+
+// Lip contour landmarks.
+const int32 kLipLeftInnerCornerIdx = 78;
+const int32 kLipRightInnerCornerIdx = 308;
+const std::vector<int32> kLipUpperIdx{82, 13, 312};
+const std::vector<int32> kLipLowerIdx{87, 14, 317};
+
+// Landmarks size
+const int32 kFaceMeshLandmarks = 468;
+
+// This calculator tracks the lip motion and detects active speakers in the images.
+// Lip contour is obtained from face mesh. The output is speakers' face bound boxes. 
+// Example:
+//    calculator: "LipTrackCalculator"
+//    input_stream: "LANDMARKS:multi_face_landmarks"
+//    input_stream: "ROIS_FROM_LANDMARKS:face_rects_from_landmarks"
+//    output_stream: "ROIS:active_speakers_rects"
+//    options:{
+//      [mediapipe.autoflip.LipTrackCalculatorOptions.ext]: {
+//        frame_history: 10
+//        iou_threshold: 0.5
+//        lip_mean_threshold: 0.3
+//        lip_variance_threshold: 0.3
+//      }
+//    }
+//
+
+class LipTrackCalculator : public CalculatorBase {
+ public:
+  LipTrackCalculator();
+  ~LipTrackCalculator() override {}
+  LipTrackCalculator(const LipTrackCalculator&) = delete;
+  LipTrackCalculator& operator=(const LipTrackCalculator&) = delete;
+
+  static ::mediapipe::Status GetContract(mediapipe::CalculatorContract* cc);
+  ::mediapipe::Status Open(mediapipe::CalculatorContext* cc) override;
+  ::mediapipe::Status Process(mediapipe::CalculatorContext* cc) override;
+
+ private:
+  // Obtain lip statistics from face landmarks
+  void GetStatistics(const std::vector<NormalizedLandmarkList>& landmark_lists, 
+                    std::vector<float>* lip_statistics); 
+  // Find match face from last frame. If not find, return -1, otherwise
+  // return the face index.
+  int MatchFace(const NormalizedRect& face_bbox);
+  // Determine whether the face is active speaker or not.
+  bool IsActiveSpeaker(const std::deque<float>& face_lip_statistics);
+  // Calculator Euclidean distance between two landmarks.
+  float GetDistance(const NormalizedLandmark& mark_1, const NormalizedLandmark& mark_2);
+  // Calculator IOU of two face bboxes.
+  float GetIOU(const NormalizedRect& bbox_1, const NormalizedRect& bbox_2);
+   
+  // Calculator options.
+  LipTrackCalculatorOptions options_;
+  // Face bounding boxes in last frame.
+  std::vector<NormalizedRect> face_bbox_;
+  // Face statistics in previous frames.
+  std::map<int32, std::deque<float>> face_statistics_;
+
+}; // end with inheritance
+
+REGISTER_CALCULATOR(LipTrackCalculator);
+
+LipTrackCalculator::LipTrackCalculator() {}
+
+::mediapipe::Status LipTrackCalculator::GetContract(
+    mediapipe::CalculatorContract* cc) {
+  if (cc->Inputs().HasTag(kInputLandmark)) {
+    cc->Inputs().Tag(kInputLandmark).Set<std::vector<NormalizedLandmarkList>>();
+  }
+  if (cc->Inputs().HasTag(kInputROI)) {
+    cc->Inputs().Tag(kInputROI).Set<std::vector<NormalizedRect>>();
+  }
+  cc->Outputs().Tag(kOutputROI).Set<std::vector<NormalizedRect>>();
+
+  return ::mediapipe::OkStatus();
+}
+
+::mediapipe::Status LipTrackCalculator::Open(
+    mediapipe::CalculatorContext* cc) {
+  options_ = cc->Options<LipTrackCalculatorOptions>();
+
+  return ::mediapipe::OkStatus();
+}
+
+::mediapipe::Status LipTrackCalculator::Process(
+    mediapipe::CalculatorContext* cc) {
+  auto output_bbox = ::absl::make_unique<std::vector<NormalizedRect>>();
+  std::vector<NormalizedRect> cur_face_bbox;
+  std::map<int32, std::deque<float>> cur_face_statistics;
+  std::vector<float> statistics;
+
+  const auto& input_landmark_lists = 
+        cc->Inputs().Tag(kInputLandmark).Get<std::vector<NormalizedLandmarkList>>();
+  const auto& input_face_bbox =
+        cc->Inputs().Tag(kInputROI).Get<std::vector<NormalizedRect>>();
+  
+  GetStatistics(input_landmark_lists, &statistics);
+
+  for (int i = 0; i < input_face_bbox.size(); ++i) {
+    auto& bbox = input_face_bbox[i];
+    cur_face_bbox.push_back(bbox);
+    int32 cur_face_idx = cur_face_bbox.size() - 1;
+
+    int previous_face_idx = MatchFace(bbox);
+    cur_face_statistics.insert(std::pair< int32, std::deque<float> >(cur_face_idx, std::deque<float>()));
+    if (previous_face_idx != -1) {
+      for (auto& value : face_statistics_[previous_face_idx])
+        cur_face_statistics[cur_face_idx].push_back(value);
+      while (cur_face_statistics[cur_face_idx].size() > options_.frame_history() - 1)
+        cur_face_statistics[cur_face_idx].pop_front();
+    }
+    else {
+      cur_face_statistics[cur_face_idx].push_back(statistics[i]);
+    }
+
+    if (IsActiveSpeaker(cur_face_statistics[cur_face_idx]))
+      output_bbox->push_back(bbox);
+  }
+
+  // Update the history
+  face_bbox_ = cur_face_bbox;
+  face_statistics_ = cur_face_statistics;
+
+  cc->Outputs().Tag(kOutputROI).Add(output_bbox.release(), cc->InputTimestamp());
+
+  return ::mediapipe::OkStatus();
+} 
+
+float LipTrackCalculator:: GetDistance(const NormalizedLandmark& mark_1,
+                                      const NormalizedLandmark& mark_2) {
+  return std::sqrt(std::pow(mark_1.x()-mark_2.x(), 2) + std::pow(mark_1.y()-mark_2.y(), 2)
+                  + std::pow(mark_1.z()-mark_2.z(), 2));
+}
+
+void LipTrackCalculator:: GetStatistics(const std::vector<NormalizedLandmarkList>& landmark_lists, 
+                    std::vector<float>* lip_statistics) {
+  float mouth_width;
+  float mouth_height = 0.0f;
+  for (const auto& landmark_list : landmark_lists) {
+    if (landmark_list.landmark_size() < kFaceMeshLandmarks)
+      continue;
+    mouth_width = GetDistance(landmark_list.landmark(kLipLeftInnerCornerIdx),
+                              landmark_list.landmark(kLipRightInnerCornerIdx));
+    for (auto i = 0; i < kLipUpperIdx.size(); ++i) {
+      mouth_height += GetDistance(landmark_list.landmark(kLipUpperIdx[i]),
+                              landmark_list.landmark(kLipLowerIdx[i]));
+    }
+    mouth_height /= (float)kLipUpperIdx.size();
+    lip_statistics->push_back(mouth_height / mouth_width);
+  }
+}
+
+int LipTrackCalculator:: MatchFace(const NormalizedRect& face_bbox) {
+  int32 idx = -1;
+  float iou = 0, maxi_iou = 0;
+  for (auto i = 0; i < face_bbox_.size(); ++i) {
+    auto& bbox = face_bbox_[i];
+    iou = GetIOU(bbox, face_bbox);
+    if (iou < options_.iou_threshold())
+      continue;
+    if (iou > maxi_iou) {
+      maxi_iou = iou;
+      idx = i;
+    }
+  }
+  return idx;
+}
+
+float LipTrackCalculator:: GetIOU(const NormalizedRect& bbox_1, const NormalizedRect& bbox_2) {
+  cv::RotatedRect cv_bbox_1, cv_bbox_2;
+  cv_bbox_1.center = cv::Point2f(bbox_1.x_center(), bbox_1.y_center());
+  cv_bbox_1.size = cv::Size2f(bbox_1.width(), bbox_1.height());
+  // TODO doubel check the rotation. Angle of cv::RotatedRect is degree
+  // while angle of NormalizedRect is NormalizedRect. Both clockwise.
+  cv_bbox_1.angle = 180.0f * bbox_1.rotation() / (float)M_PI;
+  cv_bbox_2.center = cv::Point2f(bbox_2.x_center(), bbox_2.y_center());
+  cv_bbox_2.size = cv::Size2f(bbox_2.width(), bbox_2.height());
+  cv_bbox_2.angle = 180.0f * bbox_2.rotation() / (float)M_PI;
+
+  // Get the vertices of intersecting region. 
+  std::vector<cv::Point2f> intersecting_region;
+  cv::rotatedRectangleIntersection(cv_bbox_1, cv_bbox_2, intersecting_region);
+  
+  return cv::contourArea(intersecting_region);
+}
+
+bool LipTrackCalculator:: IsActiveSpeaker(const std::deque<float>& face_lip_statistics) {
+  // If a face only appears in a few frames, it's not an active speaker. 
+  if (face_lip_statistics.size() < options_.frame_history() / 2)
+    return false;
+  float mean = 0.0f, variance = 0.0f;
+  for (auto& value : face_lip_statistics) 
+    mean += value;
+  mean /= (float)face_lip_statistics.size();
+  
+  for (auto& value : face_lip_statistics) 
+    variance += std::pow(value - mean, 2);
+  variance /= (float)face_lip_statistics.size();
+
+  // TODO determine the lip_mean_threshold and 
+  // lip_variance_threshold with experiments.
+  if (mean > options_.lip_mean_threshold() 
+    && variance > options_.lip_variance_threshold())
+    return true;
+  return false;
+}
+
+
+}  // namespace autoflip
+}  // namespace mediapipe
---
+++ autoflip_calculators/active_speaker_detection/lip_track_calculator_test.cc
@@ -0,0 +1,157 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "absl/strings/string_view.h"
+#include "mediapipe/examples/desktop/autoflip/autoflip_messages.pb.h"
+#include "mediapipe/examples/desktop/autoflip/calculators/lip_track_calculator.pb.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/calculator_runner.h"
+#include "mediapipe/framework/formats/landmark.pb.h"
+#include "mediapipe/framework/formats/rect.pb.h"
+#include "mediapipe/framework/port/gmock.h"
+#include "mediapipe/framework/port/gtest.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
+#include "mediapipe/framework/port/ret_check.h"
+#include "mediapipe/framework/port/status.h"
+#include "mediapipe/framework/port/status_matchers.h"
+#include "mediapipe/framework/port/opencv_core_inc.h"
+
+
+namespace mediapipe {
+namespace autoflip {
+namespace {
+
+constexpr char kInputLandmark[] = "LANDMARKS";
+constexpr char kInputROI[] = "ROIS_FROM_LANDMARKS";
+constexpr char kOutputROI[] = "ROIS";
+
+const int32 kFaceMeshLandmarks = 468;
+
+// Lip contour landmarks.
+const std::vector<int32> kLandmarksIdx{78, 308, 82, 13, 312, 87, 14, 317};
+// x,y,z of 6 landmarks
+const std::map< int32, std::vector<float> > kLandmaksValueTrivial{
+  {78, {0,0,0}}, 
+  {308, {0,0,0}}, 
+  {82, {0,0,0}}, 
+  {13, {0,0,0}},
+  {312, {0,0,0}}, 
+  {87, {0,0,0}}, 
+  {14, {0,0,0}}, 
+  {317, {0,0,0}}
+  };
+const std::vector<float> kRoiValueTrivial{0.5, 0.4, 0.2, 0.6, 0.0};
+
+
+constexpr char kConfig[] = R"(
+    calculator: "LipTrackCalculator"
+    input_stream: "LANDMARKS:multi_face_landmarks"
+    input_stream: "ROIS_FROM_LANDMARKS:face_rects_from_landmarks"
+    output_stream: "ROIS:active_speakers_rects"
+    options: {
+      [mediapipe.autoflip.LipTrackCalculatorOptions.ext]: {
+        frame_history: 1
+        iou_threshold: 0.5
+        lip_mean_threshold: 0.3
+        lip_variance_threshold: 0.3
+      }
+    })";
+
+NormalizedLandmark CreateLandmark(const float x, const float y, const float z) {
+  NormalizedLandmark landmark;
+  landmark.set_x(x);
+  landmark.set_y(y);
+  landmark.set_z(z);
+  return landmark;
+}
+
+void CreateLandmarkList(const std::map< int32, std::vector<float> >& values, NormalizedLandmarkList* list) {
+  for (int i = 0; i < kFaceMeshLandmarks; ++i) 
+    *list->add_landmark() = CreateLandmark(0.0f, 0.0f, 0.0f);
+
+  // TODO There is error here. Need to fix it.
+  // for (auto& idx : kLandmarksIdx){
+  //   list->landmark(idx).set_x(values[idx][0]);
+  //   // list->landmark(idx)->set_y(values[idx][1]);
+  //   // list->landmark(idx)->set_z(values[idx][2]);
+  // }
+}
+
+void CreateRoi(const std::vector<float>& values, NormalizedRect* roi) {
+  roi->set_x_center(values[0]);
+  roi->set_y_center(values[1]);
+  roi->set_width(values[2]);
+  roi->set_height(values[3]);
+  roi->set_rotation(values[4]);
+}
+
+CalculatorGraphConfig::Node MakeConfig(const std::string base_config) {
+  auto config = ParseTextProtoOrDie<CalculatorGraphConfig::Node>(base_config);
+  return config;
+}
+
+void SetInputs(const int32 landmark_num, const std::map< int32, std::vector<float> >& landmark_value, 
+          const int32 roi_num, const std::vector<float>& roi_value, CalculatorRunner* runner) {
+  // Setup landmarks
+  auto vec_landmarks_list = absl::make_unique<std::vector<NormalizedLandmarkList>>();
+  for (int i = 0; i < landmark_num; ++i) {
+    NormalizedLandmarkList landmarks_list;
+    CreateLandmarkList(landmark_value, &landmarks_list);
+    vec_landmarks_list->push_back(landmarks_list);
+  }
+  runner->MutableInputs()->Tag(kInputLandmark).packets.push_back(
+      Adopt(vec_landmarks_list.release()).At(Timestamp::PostStream()));
+  
+  // Setup ROIS
+  auto vec_roi = absl::make_unique<std::vector<NormalizedRect>>();
+  for (int i = 0; i < roi_num; ++i) {
+    NormalizedRect roi;
+    CreateRoi(roi_value, &roi);
+    vec_roi->push_back(roi);
+  }
+  runner->MutableInputs()->Tag(kInputROI).packets.push_back(
+      Adopt(vec_roi.release()).At(Timestamp::PostStream()));
+}
+
+
+void CheckOutputs(const int32 gt_output_num, const std::vector<float>& roi_value, CalculatorRunner* runner) {
+  const std::vector<Packet>& output_packets =
+      runner->Outputs().Tag(kOutputROI).packets;
+  ASSERT_EQ(1, output_packets.size());
+  
+  const auto& bboxes = output_packets[0].Get<std::vector<NormalizedRect>>();
+  ASSERT_EQ(gt_output_num, bboxes.size());
+  for (auto& bbox : bboxes) {
+    ASSERT_EQ(roi_value[0], bbox.x_center());
+    ASSERT_EQ(roi_value[1], bbox.y_center());
+    ASSERT_EQ(roi_value[2], bbox.width());
+    ASSERT_EQ(roi_value[3], bbox.height());
+    ASSERT_EQ(roi_value[4], bbox.rotation());
+  }
+}
+
+
+// One landmarksList, all the landmarks' value and ROIs' value are the same.
+TEST(LipTrackCalculatorTest, SameOneLandmarkList) {
+  auto runner = ::absl::make_unique<CalculatorRunner>(MakeConfig(kConfig));
+  int32 landmark_num = 1, roi_num = 1, gt_output_num = 0;
+  SetInputs(landmark_num, kLandmaksValueTrivial, roi_num, kRoiValueTrivial, runner.get());
+  MP_ASSERT_OK(runner->Run());
+  CheckOutputs(gt_output_num, kRoiValueTrivial, runner.get());
+}
+
+
+}  // namespace
+}  // namespace autoflip
+}  // namespace mediapipe
