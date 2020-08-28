d77e889cce79774764d50da673191a7128411aed
Jing Wang <janejwang@google.com>
2020-08-28 17:43:17 +0000

    Add missing package.json

--- web_autoflip_demo/autoflip_demo_UI/jest.config.js
+++ web_autoflip_demo/autoflip_demo_UI/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globalSetup: './setup.js',
-  globalTeardown: './teardown.js',
-  testEnvironment: './puppeteer_environment.js',
   globals: {
     URL: 'http://localhost:4444',
   },
